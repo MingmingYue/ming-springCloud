4b4e0d731751944cf1c59902d0aef47a3e81e752
MingmingYue <zhoumingming@1zhen.com>
2020-08-04 17:22:46 +0800

    1 项目初始化

--- com-gateway-service/src/main/java/com/service/gateway/GatewayApplication.java
+++ com-gateway-service/src/main/java/com/service/gateway/GatewayApplication.java
@@ -1,13 +1,16 @@
 package com.service.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.OAuth2AutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
@@ -15,9 +18,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
  * Create on 2020-07-05.
  */
 @EnableZuulProxy
-@SpringCloudApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @ComponentScan(basePackages = {"com.service"})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, OAuth2AutoConfiguration.class, SecurityFilterAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class GatewayApplication {
 
     public static void main(String[] args) {
