3125e6f4408383e464167317220b3f08d66f0a21
MingmingYue <zhoumingming@1zhen.com>
2020-08-07 19:14:44 +0800

    1 添加课程

--- com-user-service/src/main/java/com/service/user/UserApplication.java
+++ com-user-service/src/main/java/com/service/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.service.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @author xiaoMing
  * Create on 2020-08-03.
  */
+@EnableHystrix
 @EnableFeignClients
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.service", "com.core.config.register"})
