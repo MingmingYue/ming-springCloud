7e09d5e6552c6318ae287d264889e01a4c43d5c3
MingmingYue <zhoumingming@1zhen.com>
2020-08-07 10:33:07 +0800

    1 项目搭建

--- com-user-service/src/main/java/com/service/user/controller/UserController.java
+++ com-user-service/src/main/java/com/service/user/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 /**
  * @author xiaoMing
  * Create on 2020-08-04.
@@ -16,6 +18,7 @@ public class UserController {
 
     @GetMapping(value = "/details/{userId}")
     public UserModel accept(@PathVariable(value = "userId") Integer userId) {
+        System.out.println("====================" + userId + new Random().nextInt(100));
         return UserModel.builder().userId(userId).username("Ming").build();
     }
 }
