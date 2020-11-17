925eec8ea5b05127b3aaff1b02a5bef4d50c0817
MingmingYue <zhoumingming@1zhen.com>
2020-11-17 16:34:36 +0800

    1 配置

--- com-gateway-service/src/main/java/com/service/GatewayApplication.java
+++ com-gateway-service/src/main/java/com/service/GatewayApplication.java
@@ -3,14 +3,7 @@ package com.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
